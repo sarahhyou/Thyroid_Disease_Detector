7688f60fc46afb79a90b5c6dd1f8f5a6998d8cf7
Seneca <www.iamsayourah@gmail.com>
2022-12-04 13:50:35 -0500

    test vscode features

--- rough_model.py
+++ rough_model.py
@@ -3,3 +3,5 @@ import pandas as pd
 train_df = pd.read_csv('hypothyroid.csv')
 
 print(train_df)
+
+print('The mitochondria generates energy through ATP synthesis using the electron transport chain')

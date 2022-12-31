6f188aff0150fd4e0b3fef9f353ca6fc941614f1
Seneca <www.iamsayourah@gmail.com>
2022-12-31 00:12:29 -0500

    Updated file to read from new 'hypothyroid_ver2' csv file Also made some progress on data preprocessing lol

--- rough_model.py
+++ rough_model.py
@@ -1,7 +1,23 @@
 import pandas as pd
 
-train_df = pd.read_csv('hypothyroid.csv')
+# import csv dataset
+thyroid_df = pd.read_csv('hypothyroid.csv')
 
-print(train_df)
+# check composition of dataset (number of sick cases vs. non-sick cases)
+percent_sick = thyroid_df.sick.value_counts()['t']/len(thyroid_df.index) * 100
+print(thyroid_df.sick.value_counts(), f"{percent_sick:.3}%")
 
-print('The mitochondria generates energy through ATP synthesis using the electron transport chain')
+# well we know the dataset is moderately unbalanced (4% entries sick). Thus, our results could be significantly improved if we employ oversampling on the training set.
+# Let's first split up the dataset into training and testing sets
+from sklearn.model_selection import train_test_split
+
+thyroid_features, thyroid_result, X_test, Y_test = train_test_split(thyroid_df.drop(['sick'], axis= 1),
+    thyroid_df['sick'], test_size = .15, random_state = 123)
+# Further split up training set into 
+X_train, Y_train, X_val, Y_val = train_test_split(thyroid_features, thyroid_result, test_size= .15, random_state= 123)
+
+# Before we employ oversampling, the dataset contains a lot of a. missing points and b. categorical variables that need to be preprocessed.
+# 
+
+# 1. Random oversampling method:
+# import packages

1a747666eddce79859e8b4097dba12b42648b77e
Seneca <www.iamsayourah@gmail.com>
2023-01-03 22:21:01 -0500

    oversampled data using random and SMOTE methods

--- rough_model.py
+++ rough_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # import csv dataset
 thyroid_df = pd.read_csv('hypothyroid_ver2.csv')
@@ -17,23 +18,53 @@ thyroid_df['thyroid_class'].replace(['hypothyroid', 'negative'], [1,0], inplace=
 from sklearn.model_selection import train_test_split
 
 thyroid_features, X_test, thyroid_result, Y_test = train_test_split(thyroid_df.drop(['thyroid_class'], axis= 1),
-    thyroid_df['thyroid_class'], test_size = .15, random_state = 123)
+    thyroid_df['thyroid_class'], test_size = .1, random_state = 123)
 
 # Further split up non-testing set into training and validation sets
 
-X_train, Y_train, X_val, Y_val = train_test_split(thyroid_features, thyroid_result, test_size= .15, random_state= 123)
+X_train, Y_train, X_val, Y_val = train_test_split(thyroid_features, thyroid_result, test_size= .1, random_state= 123)
 
 # Before we employ oversampling, the dataset contains a lot of a. missing points and b. categorical variables that need to be preprocessed.
 # 1. Missing Data
 # The dataset contains many entries with missing data. There are two types of missing data: 
 # partially missing (some data points missing in a column)
 # and wholly missing (entire column has no data, as is the case of TBG)
-# remove TBG measured & TBG as they give no useful information
-# Also helpful to remove columns related to thyroxine, antithyroid medication, surgery, lithium, and goitre to prevent data leakeage.
+# remove TSH_measured, T3_measured, TT4_measured, T4U_measured, FTI_measured, TBG_measured and TBG as they give no useful information
+# Also helpful to remove columns related to thyroxine, antithyroid medication, surgery, lithium, and goitre to prevent data leakeage.  
 
-print(list(X_train.columns))
-X_train = X_train.drop(['TBG_measured', 'TBG', 'thyroxine', 'query_thyroxine', 'antithyroid_meds', 'thyroid_surgery', 'lithium', 'goitre'], axis= 1)
-print(list(X_train.columns))
+X_train = X_train.drop(['TSH_measured','T3_measured','TT4_measured','T4U_measured','FTI_measured','TBG_measured', 'TBG', 'thyroxine', 
+'query_thyroxine', 'query_hyperthyroid', 'query_hypothyroid','antithyroid_meds', 'thyroid_surgery', 'lithium', 'goitre'], axis= 1)
+
+# Now we deal with columns with only partially missing data. First we replace `?` with np.NaN and correctly recast data types:
+
+X_train = X_train.replace('?',np.nan)
+X_train[['age', 'TSH', 'T3', 'TT4', 'T4U','FTI']] = X_train[['age', 'TSH', 'T3', 'TT4', 'T4U','FTI']].apply(pd.to_numeric)
+
+# Then we split the reminaing dataset into categorical and numerical columns and use SimpleImputer to fill in missing numerical values
+
+categorical_cols = [cname for cname in X_train.columns if X_train[cname].dtype == "object"]
+numerical_cols = [cname for cname in X_train.columns if X_train[cname].dtype in ['int64', 'float64']]
+X_train_num = X_train[numerical_cols].copy()
+X_train_cat = X_train[categorical_cols].copy()
+
+# Because there is quite a number of data missing in each numerical column (100+ ~ 500+) 
+# I decided on using MICE (Multivariate Imputation by Chained Equation) based on a Bayesian Ridge model 
+# to maintain random variability and capture the relationship between thyroid hormone levels and age
+
+from sklearn.experimental import enable_iterative_imputer # Must keep this line to enable MICE Imputer
+from sklearn.impute import IterativeImputer
+from sklearn import linear_model
+
+mice_imputer = IterativeImputer(estimator= linear_model.BayesianRidge(), n_nearest_features= None, imputation_order= 'ascending')
+X_train_num = pd.DataFrame(mice_imputer.fit_transform(X_train_num), columns = numerical_cols)
+
+# Only a few data points were missing in the categorical columns
+# Considering that women are more at risk of thyroid issues than men, more women are likely to be surveyed for this dataset
+
+X_train_cat['sex'] = X_train_cat['sex'].fillna('F')
+print(X_train_cat.isna().sum())
+
+# Transform categorical variables
 
 # 1. Random oversampling method:
 # import packages
